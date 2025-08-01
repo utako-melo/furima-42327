539246c4179f6d739aef88fb51b93339f041e272
UTAko <rui.t.tc195@gmail.com>
2025-08-01 14:29:37 +0900

    Userモデルの単体テスト

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname {Faker::Name.name}
+    email {Faker::Internet.email}
+    password {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    last_name { Gimei.last }
+    first_name { Gimei.first }
+    first_name_kana { Gimei.first.katakana }
+    last_name_kana { Gimei.last.katakana }
+    birth_date { Faker::Date.between(from: '1930-01-01', to: '2015-12-31  ')}
+  end
+end
