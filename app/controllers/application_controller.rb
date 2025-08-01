de6a71ef8163a17a03d0870ab6c8323a7c9292cc
UTAko <rui.t.tc195@gmail.com>
2025-08-01 22:29:01 +0900

    Rubocop実施と日付設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,13 +6,15 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
     end
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :password, :password_confirmation, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth_date])
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:nickname, :email, :password, :password_confirmation,
+                                             :first_name, :last_name, :first_name_kana, :last_name_kana,
+                                             :birth_date])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
   end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,8 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname, :birth_date
-    validates :last_name, :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "全角（漢字・ひらがな・カタカナ）で入力してください"}
-    validates :last_name_kana, :first_name_kana, format: {with: /\A[ァ-ヶー]+\z/, message: "全角カタカナで入力してください"}
+    validates :last_name, :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角（漢字・ひらがな・カタカナ）で入力してください' }
+    validates :last_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: '全角カタカナで入力してください' }
   end
-  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "半角英数字混合で入力してください"}
-
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: '半角英数字混合で入力してください' }
 end
