cc572ddd9600545b1db140af7985ded85c447982
UTAko <rui.t.tc195@gmail.com>
2025-08-04 16:55:47 +0900

    ActiveStorageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,11 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_costs_id,
+                                 :prefecture_id, :delivery_time_id, :price, :image
+    ).merge(user_id: current_user.id)
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Furima42327
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
     #config.i18n.default_locale = :ja
+    config.active_storage.variant_processor = :mini_magick
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
