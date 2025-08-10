d987822580cf65fae9f134960481d21acf42396f
UTAko <rui.t.tc195@gmail.com>
2025-08-10 19:34:56 +0900

    販売手数料・利益計算のJavaScript実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,21 @@
+const price = () => {
+  window.addEventListener('turbo:load', () => {
+    console.log("OK");
+  });
+
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1)
+
+    const profitDom = document.getElementById("profit");
+    profitDom.innerHTML = Math.floor(inputValue * 0.9)
+
+  })
+
+};
+
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render", price);
