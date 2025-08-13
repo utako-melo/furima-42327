644995e9e0a2d6c3fc6a6e24766130b071e0f87e
UTAko <rui.t.tc195@gmail.com>
2025-08-13 23:31:42 +0900

    商品情報更新機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,6 +26,14 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to @item
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
 
 
   private
