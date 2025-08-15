d7a1d189a32c31bff556ba65a4ba4829a0666d86
UTAko <rui.t.tc195@gmail.com>
2025-08-15 23:45:06 +0900

    コントローラーへの条件設定を追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :redirect_unless_seller, only: [:edit, :update]
+  before_action :redirect_unless_seller, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.all.order('created_at desc')
