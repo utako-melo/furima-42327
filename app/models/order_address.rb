cdd5fcdb758ac73a5b4898e56d05bcf43221afcc
UTAko <rui.t.tc195@gmail.com>
2025-08-19 21:39:02 +0900

    Formオブジェクトの作成

---
+++ app/models/order_address.rb
@@ -0,0 +1,16 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :item_id, :user_id, :post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number
+
+  with_options :presence: true do
+    validates :item_id, :user_id, :city, :house_number
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "は10桁または11桁の半角数字で入力してください" }
+  end
+
+  def save
+    order = Order.create(item_id: item_id, user_id: user_id)
+    Address.create(post_code: post_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
+  end
+end
