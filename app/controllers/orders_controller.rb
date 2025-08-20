5f1709adca2dfc8443dfe84d2b477c23548cd818
UTAko <rui.t.tc195@gmail.com>
2025-08-20 21:30:06 +0900

    購入画面の表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,33 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
+
   def index
     @order_address = OrderAddress.new
+  end
+
+  def new
+    @order_address = OrderAddress.new
+  end
+
+  def create
+    binding.pry
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id]) 
+  end
+
+  def order_params
+    params.require(:order_address).permit(:post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -2,7 +2,7 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :item_id, :user_id, :post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number
 
-  with_options :presence: true do
+  with_options presence: true do
     validates :item_id, :user_id, :city, :house_number
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
